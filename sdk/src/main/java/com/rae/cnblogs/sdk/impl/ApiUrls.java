5edbe3bdef774ff0e4c26840970256d769a4b3e4
Rae <798404374@qq.com>
2016-12-13 00:35:49 +0800

    LOGO 更换 修复分类类型错误

--- sdk/src/main/java/com/rae/cnblogs/sdk/impl/ApiUrls.java
+++ sdk/src/main/java/com/rae/cnblogs/sdk/impl/ApiUrls.java
@@ -4,7 +4,14 @@ package com.rae.cnblogs.sdk.impl;
  * 接口地址
  * Created by ChenRui on 2016/11/30 0030 16:46.
  */
-public final class ApiUrls {
-    public static final String API_URL_HOME = "http://www.cnblogs.com/mvc/AggSite/PostList.aspx";
-    public static final String API_URL_CONTENT = "http://wcf.open.cnblogs.com/blog/post/body/";
+final class ApiUrls {
+
+    // 博客列表
+    static final String API_URL_HOME = "http://www.cnblogs.com/mvc/AggSite/PostList.aspx";
+
+    // 博文
+    static final String API_URL_CONTENT = "http://wcf.open.cnblogs.com/blog/post/body/";
+
+    // 评论列表
+    static final String API_URL_COMMENT = "http://www.cnblogs.com/mvc/blog/GetComments.aspx";
 }
