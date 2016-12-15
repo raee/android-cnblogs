6698ece1a28b66e3150c7cf03bb1ab34fcf0a47c
rae <798404374@qq.com>
2016-12-15 19:36:40 +0800

    博文列表

---
+++ app/src/main/java/com/rae/cnblogs/fragment/BlogCommentFragment.java
@@ -0,0 +1,16 @@
+package com.rae.cnblogs.fragment;
+
+import com.rae.cnblogs.R;
+
+/**
+ * 评论
+ * Created by ChenRui on 2016/12/15 0015 19:22.
+ */
+public class BlogCommentFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fm_blog_comment;
+    }
+
+}
